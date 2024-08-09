a6111346cbecf293a5c35ecf1a6b6ea5a9e88576
Bilal Layache <bilal.layache@gmail.com>
2024-08-09 13:27:54 +0200

    Initial commit

---
+++ correctme/src/main/java/com/correctme/correctme/CorrectmeApplication.java
@@ -0,0 +1,13 @@
+package com.correctme.correctme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectmeApplication.class, args);
+	}
+
+}
---
+++ correctme/src/test/java/com/correctme/correctme/CorrectmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correctme.correctme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
