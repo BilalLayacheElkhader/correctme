8b94b7e8ed8891dfe2c64a31d69c5ea6b7c6a114
Bilal Layache <bilal.layache@gmail.com>
2024-08-09 15:12:21 +0200

    + Entity + Task

---
+++ correctme/src/main/java/com/correctme/correctme/model/domain/Task.java
@@ -0,0 +1,10 @@
+package com.correctme.correctme.model.domain;
+
+public class Task {
+    // enum private int sprint;
+    // enum tasca
+    // enum nivel
+    private String comments;
+    private int idUser;
+    private int idUserCorrector;
+}
---
+++ correctme/src/main/java/com/correctme/correctme/model/domain/User.java
@@ -0,0 +1,12 @@
+package com.correctme.correctme.model.domain;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private int score;
+    private boolean available;
+    //Clase clase
+    //Role role
+}
